cc5d61fdc3e5e1ab13cd21d7eb2d4a37f143d574
T Ratnayake <thilina@tratnayake.me>
2017-09-23 00:04:37 -0700

    Scaffolded the Login container. This is where the Login form will be displayed

---
+++ src/containers/login_container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoginContainer extends Component {
+  render(){
+    return (
+      <h1>Login page scaffold.</h1>
+    )
+  }
+}
+
+export default LoginContainer;
